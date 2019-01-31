b0115ba4a6e1d8af110b2562691c99d4c78ccae8
Amber Williams <Amber-Williams@users.noreply.github.com>
2019-01-31 19:48:20 +0100

    solved

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function incrementString (str) {
-
+  let num = Number(str.match(/\d+$/gi));
+  if(!num) {
+    num = 1;
+  } else {
+    num++;
+  }
+ let restStr = str.replace(/\d+$/gi, '')
+ return restStr + num.toString();
 }
 
 module.exports = incrementString;
